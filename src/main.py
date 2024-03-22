1b11a333ff48bae11dc3c2ddddb5fe53097d2326
Max Bourgeat <max.bourgeat@ensta-bretagne.org>
2024-03-22 09:36:29 -0400

    Add comments

--- src/main.py
+++ src/main.py
@@ -12,25 +12,29 @@ if __name__ == '__main__':
     print('Hello World!')
 
     # Create a knapsack problem
+    # Methode 1 : Create a knapsack problem from scratch
     #n_item = 3
     #profit = np.array([2, 3, 4])
     #weights = np.array([[12, 19, 30], [49, 40, 31]])
     #budgets = np.array([46, 76])
     # knapsack = knapsack.MultiKnapsack(n_item, profit, weights, budgets)
+
+    # Methode 2 : Load a knapsack problem from a data file
     knapsack = utils.load_instance("data/weish10")
     print(knapsack)
 
-    
-
+    # Create a bounding object
     bounding = bounding.Bounding(knapsack)
 
     init_multiplier = 10.0
     #print(knapsack.solve())
     multipliers = np.ndarray(shape=(len(knapsack.budgets)-1, knapsack.n_item), dtype=float, buffer=np.ones((len(knapsack.budgets)-1, knapsack.n_item)) * init_multiplier)
-    #print(bounding.subgradient_optimization(multipliers, 20, 5000))
-    bound = int(bounding.unsupervised_optimization(multipliers, 0.1, 1000))
     
+    bound = bounding.subgradient_optimization(multipliers, 0.1, 5)
+    print("bound subgradient_optimization : ", bound)
+    bound = int(bounding.unsupervised_optimization(multipliers, 0.1, 5))
+    print("bound unsupervised_optimization : ", bound)
    
-    print("solving without bound", knapsack.solve())
+    # print("solving without bound", knapsack.solve())
     print("solving with bound", knapsack.solve())
 
