fac4797aeadce2b1f18d56433fd02ef1c9f7b008
Dabert <darius.dabert@icloud.com>
2024-05-23 17:48:33 -0400

    update database

--- data/mknapsack/generate_dataset_pissinger.py
+++ data/mknapsack/generate_dataset_pissinger.py
@@ -9,11 +9,12 @@ def generate_knapsack(nb_items,nb_constraints, P, tightness):
     profits = ss.expon.rvs(*P, size=nb_items)
     for i in range(len(profits)):
         profits[i]=int(profits[i])
+    profits = np.random.randint(low=0,high=1000,size=nb_items)
     profits = sorted(profits,reverse=True)
     W=[]
     capacities=[]
     for j in range (0,nb_constraints):
-        weights= np.random.randint(low=0,high=100,size=nb_items)
+        weights= np.random.randint(low=0,high=1000,size=nb_items)
         weights=np.fmax(np.zeros(nb_items),weights)
         capacity=int(tightness[j]*np.sum(weights))
         W.append(weights)
@@ -26,12 +27,12 @@ def generate_knapsack(nb_items,nb_constraints, P, tightness):
 
 with open("train/pissinger/knapsacks-data-trainset.txt",'w') as f:
 
-    Ps = (1, 100)
+    Ps = (10, 150)
     # plot the fitted distribution
     nb_constraints=5
-    nb_items = 90
+    nb_items = 30
     for i in range (0, 30):
-        tightness=np.array([0.25 + 0.06 *j for j in range(nb_constraints)])
+        tightness=np.array([0.3 + 0.03 *j for j in range(nb_constraints)])
         knapsack=generate_knapsack(nb_items,5,Ps, tightness)
         line=""
         for i in range(len(knapsack)-1):
@@ -42,12 +43,12 @@ with open("train/pissinger/knapsacks-data-trainset.txt",'w') as f:
 
 with open("test/pissinger/knapsacks-data-testset.txt",'w') as f:
 
-    Ps = (1, 100)
+    Ps = (5, 300)
     # plot the fitted distribution
     nb_constraints=5
-    nb_items = 90
+    nb_items = 30
     for i in range (0, 3):
-        tightness=np.array([0.25 + 0.06 *j for j in range(nb_constraints)])
+        tightness=np.array([0.3 + 0.03 *j for j in range(nb_constraints)])
         knapsack=generate_knapsack(nb_items,5,Ps, tightness)
         line=""
         for i in range(len(knapsack)-1):
